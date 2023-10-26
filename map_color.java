8156c0ce2586db4d51cbb12596b4c2dbdf75f621
PandaforFun <121799252+PandaforFun@users.noreply.github.com>
2023-10-26 19:22:27 +0530

    new and easy map coloring

--- map_color.java
+++ map_color.java
@@ -1,40 +1,53 @@
+import java.util.*;
+
 class Main {
-    public static void dfs(int[][] graph, int startNode) {
-        int n = graph.length;
-        boolean[] visited = new boolean[n];
-        int[] stack = new int[n];
-        int top = 0;
+    public static void main(String[] args) {
+        String[] cities = {"WA", "NT", "SA", "Q", "NSW", "V", "T"};
+        int[][] adjacencyMatrix = {
+                {0, 1, 1, 0, 0, 0, 0},
+                {1, 0, 1, 1, 0, 0, 0},
+                {1, 1, 0, 1, 1, 1, 0},
+                {0, 1, 1, 0, 1, 0, 0},
+                {0, 0, 1, 1, 0, 1, 0},
+                {0, 0, 1, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0}
+        };
+        Map<String, String> cityColors = new HashMap<>();
+        for (String city : cities) {
+            cityColors.put(city, "-*-");
+        }
+        String[] colors = {"Red", "Blue", "Green"};
 
-        visited[startNode] = true;
-        stack[top++] = startNode;
+        printState(cityColors);
+        for (int i = 0; i < cities.length; i++) {
+            String city = cities[i];
+            Set<String> usedColors = new HashSet<>();
 
-        while (top > 0) {
-            int currentNode = stack[--top];
-            System.out.print((char) ('A' + currentNode) + " ");
-            for (int i = 0; i < n; i++) {
-                if (graph[currentNode][i] == 1 && !visited[i]) {
-                    visited[i] = true;
-                    stack[top++] = i;
+            for (int j = 0; j < cities.length; j++) {
+                if (adjacencyMatrix[i][j] == 1) {
+                    String neighborColor = cityColors.get(cities[j]);
+                    if (neighborColor != null) {
+                        usedColors.add(neighborColor);
+                    }
                 }
             }
+
+            for (String color : colors) {
+                if (!usedColors.contains(color)) {
+                    cityColors.put(city, color);
+                    break;
+                }
+            }
+
+            printState(cityColors);
         }
     }
 
-    public static void main(String[] args) {
-        int n = 8;
-        int[][] adjacencyMatrix = {
-                {0, 1, 0, 0, 0, 0, 0, 0},
-                {0, 0, 1, 0, 0, 1, 0, 0},
-                {0, 0, 0, 0, 1, 0, 1, 1},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 1, 0, 0},
-                {1, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 1, 0, 0},
-                {1, 0, 0, 0, 0, 0, 0, 0}
-        };
-
-        int startNode = 0;
-        System.out.println("Depth-First Traversal [starting from node " + (char) ('A' + startNode) + "]:");
-        dfs(adjacencyMatrix, startNode);
+    private static void printState(Map<String, String> cityColors) {
+        System.out.println("Current Coloring:");
+        for (Map.Entry<String, String> entry : cityColors.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+        System.out.println("------------------");
     }
 }
