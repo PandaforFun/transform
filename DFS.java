33aff16467f88612e815f11ecf08e826b073376e
PandaforFun <121799252+PandaforFun@users.noreply.github.com>
2023-10-26 19:16:52 +0530

    new and easy DFS

--- DFS.java
+++ DFS.java
@@ -1,6 +1,18 @@
 class Main {
-    public static void dfs(int[][] graph, int startNode) {
-        int n = graph.length;
+    public static void main(String[] args) {
+        int n = 8;
+        int[][] adjacencyMatrix = {
+            {0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 1},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        int startNode = 0;
         boolean[] visited = new boolean[n];
         int[] stack = new int[n];
         int top = 0;
@@ -12,29 +24,11 @@ class Main {
             int currentNode = stack[--top];
             System.out.print((char) ('A' + currentNode) + " ");
             for (int i = 0; i < n; i++) {
-                if (graph[currentNode][i] == 1 && !visited[i]) {
+                if (adjacencyMatrix[currentNode][i] == 1 && !visited[i]) {
                     visited[i] = true;
                     stack[top++] = i;
                 }
             }
         }
-    }
-
-    public static void main(String[] args) {
-        int n = 8;
-        int[][] adjacencyMatrix = {
-                {0, 1, 0, 0, 0, 0, 0, 0},
-                {0, 0, 1, 0, 0, 1, 0, 0},
-                {0, 0, 0, 0, 1, 0, 1, 1},
-                {0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 1, 0, 0},
-                {1, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 1, 0, 0},
-                {1, 0, 0, 0, 0, 0, 0, 0}
-        };
-
-        int startNode = 0;
-        System.out.println("Depth-First Traversal [starting from node " + (char) ('A' + startNode) + "]:");
-        dfs(adjacencyMatrix, startNode);
     }
 }
